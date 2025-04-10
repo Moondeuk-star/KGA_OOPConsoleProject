9515bb9dbe4664e033279be98d4f7c42c09c4b35
Moondeuk-star <moondeuk1993@gmail.com>
2025-04-10 15:51:49 +0900

    Test Scene
    테스트 씬

---
+++ KGA_OOPConsoleProject/Scenes/TestScene01.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGA_OOPConsoleProject.Scenes
+{
+    public  class TestScene01: BaseScene
+    {
+        public override void Render()
+        {
+            Console.WriteLine("테스트 01 씬입니다");
+            Console.WriteLine();
+            Console.WriteLine(" 테스트 02 씬으로 이동");
+            Console.WriteLine("선택지를 입력하세요 : ");
+         
+        }
+        public override void Input()
+        { 
+            input = Console.ReadKey(true).key;
+        }
+
+        public override void Update()
+        { 
+        }
+        public override void Result()
+        {
+            switch (Input)
+            {
+                case ConsoleKey.D1:
+                    Game.ChangeScene("Test02");
+                    break;
+                case ConsoleKey.D2:
+
+            }
+        }
+    }
+}
